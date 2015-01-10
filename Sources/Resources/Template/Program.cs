2d0c7fa306a65406a5d294a51ee03ab963c9ff3b
Cesar Souza <cesarsouza@gmail.com>
2015-01-10 14:11:48 -0200

    Adding a template project for running example code sections.

---
+++ Sources/Resources/Template/Program.cs
@@ -0,0 +1,28 @@
+﻿using Accord;
+using Accord.Math;
+using Accord.Statistics;
+using Accord.Statistics.Analysis;
+using Accord.Statistics.Distributions.DensityKernels;
+using Accord.Statistics.Distributions.Fitting;
+using Accord.Statistics.Distributions.Multivariate;
+using Accord.Statistics.Distributions.Univariate;
+using Accord.Statistics.Kernels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ##CODE## //
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
